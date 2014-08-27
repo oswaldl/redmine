5e804099bb2d79c4cdcbde4774aee7f797279707
oswaldl <oswaldl@126.com>
2014-08-27 20:28:32 +0000

    reason:add two observers : project_observer, repository_observer modifier:oswaldl

--- config/application.rb
+++ config/application.rb
@@ -57,5 +57,9 @@ module RedmineApp
     if File.exists?(File.join(File.dirname(__FILE__), 'additional_environment.rb'))
       instance_eval File.read(File.join(File.dirname(__FILE__), 'additional_environment.rb'))
     end
+
+    # observers , @oswaldl
+    config.active_record.observers = :project_observer, :repository_observer
+
   end
 end
