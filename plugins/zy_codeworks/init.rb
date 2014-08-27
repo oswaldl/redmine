87b5594c2d7f409c697092cdbb4b216726602428
oswaldl <oswaldl@126.com>
2014-08-27 23:11:15 +0000

    reason:add zy_codeworks plugins modifier:oswaldl

---
+++ plugins/zy_codeworks/init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :zy_codeworks do
+  name 'Zy Codeworks plugin'
+  author 'oswaldl'
+  description 'This is a plugin for zhongyitech code works'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
