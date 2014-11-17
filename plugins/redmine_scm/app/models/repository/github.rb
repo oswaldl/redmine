fa92041e240d36cae8043dc9f371465e52f9b680
oswaldl <oswaldl@126.com>
2014-11-17 07:48:43 +0000

    fix a bug: use \A->^ and \z -> $

--- plugins/redmine_scm/app/models/repository/github.rb
+++ plugins/redmine_scm/app/models/repository/github.rb
@@ -1,7 +1,7 @@
 require_dependency File.expand_path('../../../../lib/adapters/github_adapter', __FILE__)
 
 class Repository::Github < Repository::Git
-    validates_format_of :url, :with => %r{^(https://github\.com/|git@github\.com:)[a-z0-9\-_]+/[a-z0-9\-_]+\.git$}i, :allow_blank => true
+    validates_format_of :url, :with => %r{\A(https://github\.com/|git@github\.com:)[a-z0-9\-_]+/[a-z0-9\-_]+\.git\z}i, :allow_blank => true
 
     before_save :set_local_url
     before_save :register_hook
