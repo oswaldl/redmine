a80870b22493b18a4be968ecd921153b0007a5ef
oswaldl <oswaldl@126.com>
2014-11-20 22:42:07 +0000

    add office365 imap authsource type sql

---
+++ db/patch.sql
@@ -0,0 +1,4 @@
+
+# add office365 auth_source
+insert into auth_sources(id,type,name,host,port,base_dn,onthefly_register,timeout)values(1,'AuthSourceImap','office365','mail.office365.com',993,'zhongyitech.com',1,10);
+
